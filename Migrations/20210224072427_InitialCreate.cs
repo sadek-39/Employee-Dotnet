d505d8bf1320e0e6a30db22ad285ab7c58da2102
sadek-39 <sadeksltn@gmail.com>
2021-02-28 12:26:16 +0600

    first commit

---
+++ Migrations/20210224072427_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mvc.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(250)", nullable: true),
+                    Empcode = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Position = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+        [Column(TypeName ="nvarchar(250)")]
+        public string Name { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Empcode { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Position { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Location { get; set; }
+    }
+}
